413fde0469483ae234e062d486f6aa7d0700f771
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-03-05 22:06:54 +0100

    task3

---
+++ task3.py
@@ -0,0 +1,13 @@
+"""
+===================   TASK 3   ====================
+* Name:  Euclidean algorithm
+*
+* Write a function `gcd` that will calculate
+* greatest common divisor for two integer values.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
